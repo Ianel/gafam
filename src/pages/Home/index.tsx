6fafbe21581c65a694563c8d44d77e30f26bf7bd
Ianel Tombozafy <ianell.s118@gmail.com>
2023-01-22 14:12:30 +0300

    add responsivity to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,25 +17,43 @@ const HomePage: React.FC = () => {
     ];
 
     return (
-        <div className="flex flex-col justify-around items-center h-full">
-            <img
-                className="object-cover h-1/2"
-                src={homeFirst}
-                alt="Home Tech 1"
-                loading="lazy"
-            />
-            <div className="flex flex-col gap-y-5 w-full">
-                {links.map((link: ButtonLinkProps, index: number) => {
-                    return <ButtonLink key={index} {...link} />;
-                })}
+        <>
+            <div className="flex lg:hidden flex-col justify-around items-center h-full">
+                <img
+                    className="object-cover h-1/2"
+                    src={homeFirst}
+                    alt="Home Tech 1"
+                    loading="lazy"
+                />
+                <div className="flex flex-col gap-y-5 w-full">
+                    {links.map((link: ButtonLinkProps, index: number) => {
+                        return <ButtonLink key={index} {...link} />;
+                    })}
+                </div>
+                <img
+                    className="object-cover h-1/2"
+                    src={homeSecond}
+                    loading="lazy"
+                    alt="Home Tech 2"
+                />
             </div>
-            <img
-                className="object-cover h-1/2"
-                src={homeSecond}
-                loading="lazy"
-                alt="Home Tech 2"
-            />
-        </div>
+            <div className="hidden lg:flex">
+                <div className="flex-1">
+                    <img
+                        className="w-full object-cover"
+                        src={homeFirst}
+                        alt="Home Tech 1"
+                    />
+                </div>
+                <div className="flex-1 flex justify-center items-center">
+                    <div className="flex flex-col gap-y-5 w-full">
+                        {links.map((link: ButtonLinkProps, index: number) => {
+                            return <ButtonLink key={index} {...link} />;
+                        })}
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
